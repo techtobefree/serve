6005f81696d5e8bdbc9cbf7123b63c50b6657bbd
Taylor McKay <jtmckay@gmail.com>
2024-11-03 11:53:50 -0700

    Setup layout

--- src/pages/(tabs)/index.tsx
+++ src/pages/(tabs)/index.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import { IonButton } from '@ionic/react'
 
 function App() {
@@ -8,14 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>This is stuff</h1>
       <div className="card">
         <IonButton color="success" fill="solid" onClick={() => { setCount((count) => count + 1) }}>count is {count}</IonButton>
