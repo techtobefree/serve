76a90cff04f7a84e1c52b4317a1b3efd6566eb82
Taylor McKay <jtmckay@gmail.com>
2024-11-03 14:37:21 -0700

    Extend TabButton with activeIcon interface

--- src/components/TabSelection/TabButton.tsx
+++ src/components/TabSelection/TabButton.tsx
@@ -3,24 +3,27 @@ import { IonButton, IonIcon } from "@ionic/react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-const isActive = (location: any, path: string) => location.pathname === path;
+const checkIfActive = (location: any, path: string) => location.pathname === path;
 
 type Props = {
   path: string,
   icon: string,
+  activeIcon: string,
   onClick?: () => void,
 }
 
-export default function TabButton({ path, icon, onClick }: Props) {
+export default function TabButton({ path, icon, activeIcon, onClick }: Props) {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const isActive = checkIfActive(location, path)
+
   return (
     <IonButton
-      className={`flex-1 ${isActive(location, path) ? 'text-blue-500' : 'black'}`}
+      className={`flex-1 ${isActive ? 'text-blue-500' : 'black'}`}
       onClick={onClick ? onClick : () => { navigate(path, { replace: true }) }}
     >
-      <IonIcon icon={icon} />
+      <IonIcon icon={isActive ? activeIcon : icon} />
     </IonButton>
   )
 }
--- src/components/TabSelection/TabSelection.tsx
+++ src/components/TabSelection/TabSelection.tsx
@@ -12,14 +12,14 @@ export default function TabSelection({ headerIsVisible }: Props) {
 
   return (
     <div className={`flex h-${TAB_SELECTION_HEIGHT} w-full fixed bottom-0 md:bottom-auto md:top-0 ${headerIsVisible ? 'md:translate-y-full' : 'md:translate-y-0'} left-0 right-0 bg-gray-800 text-white transition-transform duration-300 ease-in-out z-10`}>
-      <TabButton path="/" icon={home} />
-      <TabButton path="/projects" icon={people} />
-      <TabButton path="" icon={add} onClick={() => {
+      <TabButton icon={home} activeIcon={home} path="/" />
+      <TabButton icon={people} activeIcon={people} path="/projects" />
+      <TabButton icon={add} activeIcon={add} path="" onClick={() => {
         navigate('/projects', { replace: true })
         console.log('Add a project')
       }} />
-      <TabButton path="/groups" icon={people} />
-      <TabButton path="/messages" icon={search} />
+      <TabButton icon={people} activeIcon={people} path="/groups" />
+      <TabButton icon={search} activeIcon={search} path="/messages" />
     </div>
   )
 }
