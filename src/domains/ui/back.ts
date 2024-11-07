5129b0861558d770d4d25ae95af17fee2e78f782
Taylor McKay <jtmckay@gmail.com>
2024-11-06 23:17:11 -0700

    Fix back again

--- src/domains/ui/back.ts
+++ src/domains/ui/back.ts
@@ -1,10 +1,15 @@
 import { Path, useNavigate } from "../../router";
+import { DEVICE, DEVICE_TYPE } from "./device";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const navigation: any;
 
 export function back(navigate: ReturnType<typeof useNavigate>) {
   let destination: number | string = -1;
+  if (DEVICE.PLATFORM !== DEVICE_TYPE.web) {
+    navigate(destination)
+  }
+
   if (!navigation) {
     navigate(destination)
   }
