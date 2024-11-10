d9b827ad09eee48c73a489fb4c33c191dbabc0c5
Taylor McKay <jtmckay@gmail.com>
2024-11-10 11:08:27 -0700

    Clean join page

--- src/pages/(header)/project/[projectId]/join.tsx
+++ src/pages/(header)/project/[projectId]/join.tsx
@@ -31,31 +31,26 @@ export function ProjectJoinPageComponent({ userId }: Props) {
     }
   }, [data, projectId, navigate])
 
-  if (!userId) {
-    return (
-      <div>
-        <div>
-          You must login to join a project.
-        </div>
-        <IonButton onClick={() => { navigate('/project/:projectId/view', { params: { projectId }, replace: true }) }}>View project</IonButton>
-      </div>
-    )
-  }
-
   return (
     <>
       <div>
         <IonIcon className='cursor-pointer text-4xl' icon={arrowBack} onClick={() => { back(navigate) }} />
       </div>
       <div className="flex justify-center">
-        <div className="max-w-[800px] w-full">
-          {isLoading && <div>Joining project ...</div>}
-          {isError && <div>Error joining project</div>}
-          {!isError && !isLoading &&
-            <div>You should be redirected to view {projectId}</div>
-          }
-          <IonButton onClick={() => { navigate('/project/:projectId/view', { params: { projectId }, replace: true }) }}>View project</IonButton>
-        </div>
+        {userId ?
+          <div className="max-w-[800px] w-full">
+            {isLoading && <div>Joining project ...</div>}
+            {isError && <div>Error joining project</div>}
+            {!isError && !isLoading &&
+              <div>You should be redirected to view {projectId}</div>
+            }
+          </div>
+          :
+          <div>
+            You must login to join a project.
+          </div>
+        }
+        <IonButton onClick={() => { navigate('/project/:projectId/view', { params: { projectId }, replace: true }) }}>View project</IonButton>
       </div>
     </>
   )
